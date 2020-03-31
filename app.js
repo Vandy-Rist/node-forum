73a7516dfb9912768c134792c6a00ca8692b5c35
Vandy-Rist <lionfan1368@gmail.com>
2020-03-31 13:09:00 +0800

    register-page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser')
+var session = require('express-session')
 var router = require('./router')
 
 var app = express()
@@ -11,13 +12,20 @@ app.use('/views/', express.static(path.join(__dirname, './node_modules/')))
 
 // 配置模版引擎
 app.engine('html', require('express-art-template'))
-app.set('views', path.join(__dirname, './views'))
+app.set('views', path.join(__dirname, './views/'))
 
 // 配置解析表单 POST 请求体插件（注意：一定要在 app.use(router) 之前 ）
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
+// 配置 express-session
+app.use(session({
+	secret: 'itcast',
+	resave: false,
+	saveUninitialized: false
+}))
 
 // 把路由加载到 app 中
 app.use(router)
--- router.js
+++ router.js
@@ -40,6 +40,7 @@ router.post('/register', function (req, res) {
   			message: '服务端错误'
   		})
   	}
+    console.log(data)
   	if (data) {
   		// 邮箱或者昵称已存在
   		return res.status(200).json({
@@ -48,7 +49,27 @@ router.post('/register', function (req, res) {
   		})
   		return res.send('邮箱或者密码已存在，请重试')
   	}
-  	
+  	// 对密码进行 md5 重复加密
+  	body.password = md5(md5(body.password))
+
+    new User(body).save(function (err,user) {
+      if (err) {
+        return res.status(500).json({
+          err_code: 500,
+          message: 'Internet error.'
+        })
+      }
+
+      // 注册成功，使用 Session 来记录用户的登录状态
+      req.session.user = user
+
+      // Express 提供了一个响应方法：json
+      // 该方法接收一个对象作为参数，它会自动帮你把对象转为字符串再发送给浏览器
+      res.status(200).json({
+        err_code: 0,
+        message: 'OK'
+      })
+    })
 
   })
 
